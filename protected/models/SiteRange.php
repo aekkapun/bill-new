e92f285c890358fb5de711a1a34b74185117620e
Denis Boldinov <positivejob@yandex.ru>
2012-05-23 12:07:36 +0400

    bugfix

--- protected/models/SiteRange.php
+++ protected/models/SiteRange.php
@@ -17,72 +17,72 @@
  */
 class SiteRange extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return SiteRange the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
-
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'site_range';
-	}
-
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('site_id, price', 'required'),
-			array('site_id, valueMin, valueMax, price', 'length', 'max'=>10),
-            array('valueMin, valueMax', 'numerical'),
-			array('created_at, updated_at', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, site_id, valueMin, valueMax, price, created_at, updated_at', 'safe', 'on'=>'search'),
-		);
-	}
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'site' => array(self::BELONGS_TO, 'Site', 'site_id'),
-		);
-	}
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'site_id' => 'Сайт',
-			'valueMin' => 'Мин',
-			'valueMax' => 'Макс',
-			'price' => 'Цена',
-            'created_at' => 'Время создания',
-            'updated_at' => 'Время обновления',
-		);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @return SiteRange the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
     /**
-    * @return array behaviors.
-    */
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'site_range';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('site_id, price', 'required'),
+            array('site_id, valueMin, valueMax', 'length', 'max' => 10),
+            array('valueMin, valueMax, price', 'numerical'),
+            array('created_at, updated_at', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, site_id, valueMin, valueMax, price, created_at, updated_at', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'site' => array(self::BELONGS_TO, 'Site', 'site_id'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'site_id' => 'Сайт',
+            'valueMin' => 'Мин',
+            'valueMax' => 'Макс',
+            'price' => 'Цена',
+            'created_at' => 'Время создания',
+            'updated_at' => 'Время обновления',
+        );
+    }
+
+    /**
+     * @return array behaviors.
+     */
     public function behaviors()
     {
         return array(
@@ -95,27 +95,27 @@ class SiteRange extends CActiveRecord
         );
     }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('site_id',$this->site_id,true);
-		$criteria->compare('valueMin',$this->valueMin,true);
-		$criteria->compare('valueMax',$this->valueMax,true);
-		$criteria->compare('price',$this->price,true);
-		$criteria->compare('created_at',$this->created_at,true);
-		$criteria->compare('updated_at',$this->updated_at,true);
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('site_id', $this->site_id, true);
+        $criteria->compare('valueMin', $this->valueMin, true);
+        $criteria->compare('valueMax', $this->valueMax, true);
+        $criteria->compare('price', $this->price, true);
+        $criteria->compare('created_at', $this->created_at, true);
+        $criteria->compare('updated_at', $this->updated_at, true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 }
--- protected/models/User.php
+++ protected/models/User.php
@@ -137,7 +137,7 @@ class User extends CActiveRecord
         // NOTE: you may need to adjust the relation name and the related
         // class name for the relations automatically generated below.
         return array(
-            'client' => array(self::BELONGS_TO, 'Client', 'id'),
+            'client' => array(self::BELONGS_TO, 'Client', 'client_id'),
             'clientAgents' => array(self::HAS_MANY, 'ClientAgent', 'user_id'),
         );
     }
