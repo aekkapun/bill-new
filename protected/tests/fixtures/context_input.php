f5d6202422f19a36a9cab35e51e464afcbe5a38c
Denis Boldinov <positivejob@yandex.ru>
2012-07-09 15:25:32 +0400

    UPD #78, #73 Статистика по контекстной рекламе, update тестов

---
+++ protected/tests/fixtures/context_input.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: denis
+ * Date: 05.07.12
+ * Time: 14:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+return array(
+    'input1' => array(
+        'site_id' => 1,
+        'transitions_count' => 10,
+        'transitions_sum' => 150,
+        'avg_transition_price' => 15,
+        'contract_id' => 1,
+        'adv_platform_id' => 1,
+    ),
+    'input2' => array(
+        'site_id' => 1,
+        'transitions_count' => 20,
+        'transitions_sum' => 200,
+        'avg_transition_price' => 10,
+        'contract_id' => 1,
+        'adv_platform_id' => 1,
+    ),
+    'input3' => array(
+        'site_id' => 1,
+        'transitions_count' => 100,
+        'transitions_sum' => 150,
+        'avg_transition_price' => 1.5,
+        'contract_id' => 1,
+        'adv_platform_id' => 2,
+    ),
+    'input4' => array(
+        'site_id' => 1,
+        'transitions_count' => 200,
+        'transitions_sum' => 250,
+        'avg_transition_price' => 1.25,
+        'contract_id' => 1,
+        'adv_platform_id' => 2,
+    ),
+);
